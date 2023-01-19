a6e4c9dddea5ff7e21451a95db7ac4b06eedf894
Higor Cardoso Da Silva <higor.io@hotmail.com>
2023-01-19 10:48:29 -0300

    isolate the app routes

--- source/server.ts
+++ source/server.ts
@@ -1,17 +1,12 @@
 
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { routes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany()
-  return habits
-})
+app.register(routes)
 
 app.listen({
   port: 3333,
