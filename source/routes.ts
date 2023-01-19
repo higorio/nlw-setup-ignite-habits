ebdb0ec89d2e73350c3f13f6e31774cc22992043
Higor Cardoso Da Silva <higor.io@hotmail.com>
2023-01-19 11:49:04 -0300

    creates create habit route

--- source/routes.ts
+++ source/routes.ts
@@ -1,9 +1,38 @@
 import { FastifyInstance } from "fastify"
 import { prisma } from "./lib/prisma"
+import { z } from "zod"
+import daysjs from 'dayjs'
 
 export async function routes(app: FastifyInstance) {
   app.get('/', async () => {
     const habits = await prisma.habit.findMany()
     return habits
+  })
+
+  app.post('/createHabit', async (request, response) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+        z.number().min(0).max(6)
+      )
+    })
+
+    const { title, weekDays } = createHabitBody.parse(request.body)
+
+    const today = daysjs().startOf('day').toDate()
+
+    await prisma.habit.create({
+      data: {
+        title,
+        createdAt: today,
+        HabitWeekDays: {
+          create: weekDays.map(weekDays => {
+            return {
+              week_day: weekDays
+            }
+          })
+        }
+      }
+    })
   })
 }
